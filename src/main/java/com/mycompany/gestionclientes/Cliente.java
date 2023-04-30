f39daa5fd4d80d7c17e46974e07bd954a8d46bbf
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-30 19:03:06 -0300

    first commit
    Probando Java

---
+++ src/main/java/com/mycompany/gestionclientes/Cliente.java
@@ -0,0 +1,10 @@
+package com.mycompany.gestionclientes;
+
+public class Cliente {
+
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String tetefono;
+
+}
---
+++ src/main/java/com/mycompany/gestionclientes/GestionClientes.java
@@ -0,0 +1,10 @@
+package com.mycompany.gestionclientes;
+
+public class GestionClientes {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.show();
+    }
+
+}
